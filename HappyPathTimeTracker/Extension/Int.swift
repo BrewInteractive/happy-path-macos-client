3f962c674be9a4a800d8eb494df361ef6be5db9d
Orhan Gorkem Sevim <gorkemsevim@gmail.com>
2023-07-16 17:40:46 +0300

    add descriptions

--- HappyPathTimeTracker/Extension/Int.swift
+++ HappyPathTimeTracker/Extension/Int.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 extension Int {
+    /// use to convert active timer seconds to hour description
+    /// 
+    /// For example: 4567 -> 01:16:07
     var toHours: String {
         let hours = self / 3600
         let minute = (self - hours * 3600) / 60
@@ -16,15 +19,20 @@ extension Int {
         return "\(String(format: "%02d", hours)):\(String(format: "%02d", minute)):\(String(format: "%02d", seconds))"
     }
     
+    /// use to convert minute to hours coming from db
+    ///
+    /// For example: 123 -> 02:03
     var minuteToHours: String {
         let hours = self / 60
         let minute = (self - hours * 60)
-        
         return "\(String(format: "%02d", hours)):\(String(format: "%02d", minute))"
     }
 }
 
 extension Optional<Int> {
+    /// use to convert minute to hours coming from db
+    ///
+    /// For example: 123 -> 02:03
     var minuteToHours: String {
         if self == nil {
             return "00:00"
--- HappyPathTimeTracker/Extension/String.swift
+++ HappyPathTimeTracker/Extension/String.swift
@@ -8,14 +8,24 @@
 import Foundation
 
 extension String {
+    /// use to convert hour string style to minute to send db
+    ///
+    /// For example:
+    ///
+    /// 01:12 -> 72,
+    ///
+    /// :12 -> 12
     var toMinute: Int {
         if self.isEmpty {
             return 0
         }
         if self.contains(":") {
             let parts = self.split(separator: ":")
-            
-            return Int(parts[0])! * 60 + Int(parts[1])!
+            if parts.count == 2 {
+                return Int(parts[0])! * 60 + Int(parts[1])!
+            } else if parts.count == 1 {
+                return Int(parts[0])!
+            }
         }
         
         return Int(self)!
