59f34ce1782a52d96cfd353d4a956e8abd17795f
Orhan Gorkem Sevim <gorkemsevim@gmail.com>
2023-09-01 08:05:07 +0300

    remove useless code

--- HappyPathTimeTracker/Screen/Main/Views/CalendarView.swift
+++ HappyPathTimeTracker/Screen/Main/Views/CalendarView.swift
@@ -11,24 +11,16 @@ import SwiftUI
 struct CalendarView: View {
     @EnvironmentObject var mainScreenVm: MainScreenViewModel
     var calendar: Calendar
-//    private let monthDayFormatter: DateFormatter
-    private let dayFormatter: DateFormatter
-    private let weekFormatter: DateFormatter
     
     @Binding var selectedDate: Date
     
     init(selectedDate: Binding<Date>) {
         self.calendar = Calendar.current
-//        self.calendar.firstWeekday = 0
-//        self.monthDayFormatter = DateFormatter(dateFormat: "MM/dd", calendar: calendar)
-        self.dayFormatter = DateFormatter(dateFormat: "d", calendar: calendar)
-        self.weekFormatter = DateFormatter(dateFormat: "EEEEE", calendar: calendar)
         self._selectedDate = selectedDate
     }
     
     var body: some View {
-        CalendarWeekListView(calendar: calendar,
-                             date: $selectedDate) { date in
+        CalendarWeekListView(calendar: calendar, date: $selectedDate) { date in
             CircleDayView(circleDay: .init(date: date, isSelected: isSameWith(date1: selectedDate, date2: date), totalSeconds: 0), dailyTotalDuration: mainScreenVm.getTotalDurationMinuteOfDayAsString(date: date))
                 .onTapGesture {
                     mainScreenVm.updateMainScreenVmProp(for: \.selectedDate, newValue: date)
@@ -37,11 +29,11 @@ struct CalendarView: View {
                     }
                 }
         }
-                             .padding(.horizontal, 8)
-                             .padding(.vertical, 14)
-                             .background {
-                                 Color.Primary.LightBabyPowder
-                             }
+        .padding(.horizontal, 8)
+        .padding(.vertical, 14)
+        .background {
+            Color.Primary.LightBabyPowder
+        }
     }
     func isSameWith(date1: Date, date2: Date) -> Bool {
         return calendar.isDate(date1, inSameDayAs: date2)
