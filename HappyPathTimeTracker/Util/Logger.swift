b094b2ae71fde5e6cbc531a95e71a406fce380c0
Orhan Gorkem Sevim <gorkemsevim@gmail.com>
2024-02-12 23:20:40 +0300

    fetch logs to send devs

--- HappyPathTimeTracker/Util/Logger.swift
+++ HappyPathTimeTracker/Util/Logger.swift
@@ -12,12 +12,14 @@ final class HappyLogger {
     private static let subsystem = Bundle.main.bundleIdentifier!
     private static let category = "happy-macos"
     static let logger = Logger(subsystem: subsystem, category: category)
-//    static func readLogs() throws {
-//        let store = try OSLogStore.local()
-//        let startOfDay = store.position(date: .now.dateAtStartOf(.day))
-//        let entries = try store.getEntries(with: [], at: startOfDay, matching: .none)
-//        for entry in entries {
-//            print(entry.description)
-//        }
-//    }
+    static func readLogs() throws {
+        let store = try OSLogStore(scope: .currentProcessIdentifier)
+        let startOfDay = store.position(date: Date().dateAtStartOf(.day))
+        let entries = try store.getEntries(with: [], at: startOfDay, matching: .none)
+            .compactMap({$0 as? OSLogEntryLog})
+            .filter({$0.subsystem == subsystem})
+        for entry in entries {
+            print("\(entry.date.toISO()): message: \(entry.composedMessage)")//TODO: create an email template with these and send to the happy path macos team
+        }
+    }
 }
